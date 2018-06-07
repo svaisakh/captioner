294594f7fa5e29904fae72452f41e35d79bba891
Vaisakh <svaisakh1994@gmail.com>
2018-06-07 13:34:27 +0530

    Implement Beam Search and Refactor Model Sampling

--- captioner/model.py
+++ captioner/model.py
@@ -4,46 +4,71 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 
+from captioner.nlp import idx_word
+
 class Model(nn.Module):
     def __init__(self, feature_dim, embedding_dim, hidden_size, num_layers, rnn_type, vocab_size):
+        from captioner.utils import BeamSearch
+
         super().__init__()
         self.fc_feat = nn.Linear(feature_dim, hidden_size * num_layers)
         rnn_module_dict = {'lstm': nn.LSTM, 'rnn': nn.RNN}
         self.rnn = rnn_module_dict[rnn_type](embedding_dim, hidden_size, num_layers, batch_first=True)
         self.fc = nn.Linear(hidden_size, vocab_size + 1)
+
+        self._search = BeamSearch()
+
         self.to(mag.device)
 
-    def forward(self, feat, cap=None, nlp=None, max_len=20):
-        h0 = F.relu(self.fc_feat(feat)).view(1, self.rnn.num_layers, -1).transpose(0, 1).contiguous()
-        if isinstance(self.rnn, nn.LSTM): h0 = (h0, torch.zeros_like(h0))
-
+    def forward(self, features, cap=None, nlp=None, beam_size=3, max_len=20):
         if cap is None:
             if self.training: raise ValueError('Provide caption while training')
-            return self._generate(h0, nlp, max_len)
+            return self._generate(features, nlp, beam_size, max_len)
 
+        h0 = self._get_initial_hidden(features)
         x = self.rnn(cap, h0)[0]
         return self.fc(x)
 
-    def _generate(self, h, nlp, max_len):
-        y = torch.zeros(1, max_len).to(self.device)
-        x = torch.tensor(nlp('`')[0].vector).view(1, 1, -1).to(self.device)
+    def _generate(self, features, nlp, beam_size, max_len):
+        self._search.build = lambda *args: self._build(*args, nlp=nlp)
+        branches = self._search(beam_size, features, max_len)
 
-        for i in range(max_len): y[0, i], h, x = self._sample(x, h, nlp)
+        captions, probs = [], []
+        for branch in branches:
+            indices, prob = branch.content, branch.score
+            caption = ' '.join([idx_word(idx, nlp) for idx in indices])
+            caption = caption[:caption.find('.')]
+            if caption not in captions:
+                captions.append(caption)
+                probs.append(prob)
 
-        return y
+        return list(zip(captions, probs))
 
-    def _sample(self, x, h, nlp):
-        x, h = self.rnn(x, h)
-        x = self.fc(x).squeeze(1)
-        idx = x.max(-1)[1]
-
-        if idx == self.fc.out_features - 1:
-            next_vector = torch.zeros(1, 1, embedding_dim).to(self.device)
+    def _build(self, content, context, nlp):
+        prev_ids = content
+        if len(prev_ids) == 0:
+            x = torch.tensor(nlp('`')[0].vector).view(1, 1, -1).to(self.device)
+            h = self._get_initial_hidden(context)
         else:
-            hash_val = nlp.vocab.vectors.find(row=idx.item())[0]
-            next_vector = torch.tensor(nlp.vocab.vectors[hash_val]).to(self.device).view(1, 1, -1)
+            h = context
+            last_idx = prev_ids[-1]
+            if last_idx == self.fc.out_features - 1:
+                x = torch.zeros(1, 1, self.rnn.input_size).to(self.device)
+            else:
+                hash_val = nlp.vocab.vectors.find(row=last_idx)[0]
+                x = torch.tensor(nlp.vocab.vectors[hash_val]).to(self.device).view(1, 1, -1)
 
-        return idx, h, next_vector
+        x, context = self.rnn(x, h)
+        log_probs = F.log_softmax(self.fc(x).view(-1), dim=-1)
+        content, scores  = list(zip(*[(i, p.item()) for i, p in enumerate(log_probs)]))
+
+        return content, scores, context
+
+    def _get_initial_hidden(self, features):
+        h0 = F.relu(self.fc_feat(features)).view(1, self.rnn.num_layers, -1).transpose(0, 1).contiguous()
+        if isinstance(self.rnn, nn.LSTM): h0 = (h0, torch.zeros_like(h0))
+
+        return h0
 
     def to(self, *args, **kwargs):
         super().to(*args, **kwargs)
--- captioner/utils.py
+++ captioner/utils.py
@@ -1,69 +1,111 @@
+import numpy as np
+
 from contextlib import contextmanager
+from collections import namedtuple
+from itertools import chain
+
+class BeamSearch:
+	Branch = namedtuple('Branch', ['content', 'score', 'context'])
+
+	def __init__(self, build=None):
+		self.build = build
+
+	def __call__(self, beam_size, context, max_len):
+		return self._search(beam_size, context, max_len)
+
+	def _search(self, beam_size, context, max_len):
+		branches = [self.Branch([], 0, context)]
+
+		for _ in range(max_len):
+			branches = list(chain(*[[new_branch
+									 for new_branch in self._get_branches(branch, beam_size)]
+									for branch in branches]))
+
+			branches.sort(key=lambda branch: branch.score, reverse=True)
+			branches = branches[:beam_size]
+
+		total_prob = sum(np.exp(branch.score) for branch in branches)
+		branches = [self.Branch(branch.content, np.exp(branch.score) / total_prob, None) for branch in branches]
+
+		return branches
+
+	def _get_branches(self, branch, beam_size):
+		contents, scores, context = self.build(branch.content, branch.context)
+		nodes = [self.Branch([content], score, context)
+				 for content, score in zip(contents, scores)]
+
+		nodes.sort(key=lambda node: node.score, reverse=True)
+		nodes = nodes[:beam_size]
+
+		return [self._merge(branch, node) for node in nodes]
+
+	def _merge(self, b1, b2):
+		return self.Branch(b1.content + b2.content, b1.score + b2.score, b2.context)
 
 def show_coco(img, captions):
-    import matplotlib.pyplot as plt
+	import matplotlib.pyplot as plt
 
-    from numpy.random import randint
+	from numpy.random import randint
 
-    captions = captions[randint(len(captions))]
+	captions = captions[randint(len(captions))]
 
-    def show_image(image, title):
-        image = image.permute(1, 2, 0).numpy().copy()
-        i_min, i_max = image.min(), image.max()
-        image = (image - i_min) / (i_max - i_min)
-        plt.imshow(image)
-        plt.xticks([]); plt.yticks([]); plt.grid(False)
-        plt.title(title)
-        plt.show()
+	def show_image(image, title):
+		image = image.permute(1, 2, 0).numpy().copy()
+		i_min, i_max = image.min(), image.max()
+		image = (image - i_min) / (i_max - i_min)
+		plt.imshow(image)
+		plt.xticks([]); plt.yticks([]); plt.grid(False)
+		plt.title(title)
+		plt.show()
 
-    for i, c in zip(img, captions): show_image(i, c)
+	for i, c in zip(img, captions): show_image(i, c)
 
 def loopy(gen):
-    while True:
-        for x in iter(gen): yield x
+	while True:
+		for x in iter(gen): yield x
 
 def working_directory(path):
-    """path can also be a function in case of decorator"""
+	"""path can also be a function in case of decorator"""
 
-    from inspect import isfunction
-    if not isfunction(path):
-        return _working_directory_context_manager(path)
+	from inspect import isfunction
+	if not isfunction(path):
+		return _working_directory_context_manager(path)
 
-    from functools import wraps
+	from functools import wraps
 
-    @wraps(path)
-    def new_fn(*args, **kwargs):
-        from pathlib import PosixPath
+	@wraps(path)
+	def new_fn(*args, **kwargs):
+		from pathlib import PosixPath
 
-        working_path = [a for a in args if type(a) is PosixPath]
-        if len(working_path) != 0: working_path = working_path[0]
-        else:
-            working_path = [v for v in kwargs.values() if type(v) is PosixPath]
-            if len(working_path) != 0: working_path = working_path[0]
-            else: raise RuntimeError('No suitable paths found')
+		working_path = [a for a in args if type(a) is PosixPath]
+		if len(working_path) != 0: working_path = working_path[0]
+		else:
+			working_path = [v for v in kwargs.values() if type(v) is PosixPath]
+			if len(working_path) != 0: working_path = working_path[0]
+			else: raise RuntimeError('No suitable paths found')
 
-        with _working_directory_context_manager(working_path):
-            return path(*args, **kwargs)
+		with _working_directory_context_manager(working_path):
+			return path(*args, **kwargs)
 
-    return new_fn
+	return new_fn
 
 @contextmanager
 def _working_directory_context_manager(path):
-    import os
+	import os
 
-    # Change to working directory
-    path_cwd = os.getcwd()
-    os.chdir(path)
+	# Change to working directory
+	path_cwd = os.getcwd()
+	os.chdir(path)
 
-    yield
+	yield
 
-    os.chdir(path_cwd) # Change back to working directory
+	os.chdir(path_cwd) # Change back to working directory
 
 def get_tqdm():
-    import tqdm
+	import tqdm
 
-    try:
-        get_ipython
-        return getattr(tqdm, 'tqdm_notebook')
-    except:
-        return getattr(tqdm, 'tqdm')
+	try:
+		get_ipython
+		return getattr(tqdm, 'tqdm_notebook')
+	except:
+		return getattr(tqdm, 'tqdm')
