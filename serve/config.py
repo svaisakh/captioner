67168746ec5e186469521c746b89b3dc65239ac2
Vaisakh <svaisakh1994@gmail.com>
2018-06-12 18:27:08 +0530

    Add Secret Key

--- serve/config.py
+++ serve/config.py
@@ -3,5 +3,5 @@ import os
 from pathlib import Path
 
 class Config:
-	SECRET_KEY = os.environ.get('SECRET_KEY') or 'supposed-to-be-secret'
+	SECRET_KEY = os.environ.get('SECRET_KEY') or b'{n\xae7Uu\xb6~Z\x899\xa6\xf1\xc3Y3\x87\x81\x17\x1c9Jx\x97'
 	UPLOAD_FOLDER = str(Path(__file__).resolve().parent / 'app' / 'uploads')
